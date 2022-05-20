55e63aac2d452435cc0cbdfa17fa548d52930286
Ghostephant <105659056+Ghostephant@users.noreply.github.com>
2022-05-20 11:56:37 -0500

    Add files via upload

---
+++ Whatever you want/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Whatever_you_want
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
