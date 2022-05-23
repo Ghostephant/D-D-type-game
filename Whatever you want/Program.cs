badf013bb1b63b0bb6ced85bb45593cb18ac4ca9
Ghostephant <105659056+Ghostephant@users.noreply.github.com>
2022-05-23 11:52:40 -0500

    Add files via upload

--- Whatever you want/Program.cs
+++ Whatever you want/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
-
+using static System.Console;
 namespace Whatever_you_want
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string response;
+            int rolledNum;
+            WriteLine("Welcome Adventurer, to Heck");
+            WriteLine("Congratulations you are dead.");
+            WriteLine("If you wish to know the circumstances with which the cessation of life has occured, say 'yes daddy please', if you wish to continue on in blissful ignorance, say 'NO GOD PLEASE NO'");
+            response = ReadLine();
+            if (response.Equals("yes daddy please"))
+            {
+                rolledNum = Roll.D6;
+
+            }
+            else if (response.Equals())
         }
     }
 }
